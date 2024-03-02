39163f8aea243c5a2659b06375726c31882aca8b
Raazi Muhammed K <raazi6163@gmail.com>
2024-03-02 10:57:04 +0530

    chore(team): setup team service

---
+++ team/src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+import { loadEnv } from "@omniflow/common";
+//import authRoutes from "./routers/index.js";
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+const app = express();
+
+const { SERVER_NAME, CORS_ORIGINS, NODE_ENV } = loadEnv([
+    "PORT",
+    "SERVER_NAME",
+    "CORS_ORIGINS",
+    "NODE_ENV",
+]);
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+    cors({
+        origin: CORS_ORIGINS,
+        credentials: true,
+    })
+);
+
+if (NODE_ENV === "production") {
+    app.use(morgan("dev"));
+}
+
+//app.use("/api/project", authRoutes);
+
+app.all("*", (req, res) => {
+    console.log(`@${SERVER_NAME}`, req.method, req.originalUrl);
+    res.status(404).json({
+        message: `Reached ${SERVER_NAME} service`,
+    });
+});
+
+export default app;
