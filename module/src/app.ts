ca1f36dc8a313adefa598621b0b4ed9ecba8bd7e
Raazi Muhammed K <raazi6163@gmail.com>
2024-03-16 11:41:33 +0530

    chore(module): setup module service

---
+++ module/src/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+import { ErrorHandlingMiddleware, loadEnv, logger } from "@omniflow/common";
+// import { authRoutes, userRoutes } from "./routers/index.js";
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+const app = express();
+
+const { CORS_ORIGINS } = loadEnv([
+    "PORT",
+    "SERVER_NAME",
+    "CORS_ORIGINS",
+    "NODE_ENV",
+    "ACCESS_TOKEN_SECRET",
+]);
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+    cors({
+        origin: CORS_ORIGINS,
+        credentials: true,
+    })
+);
+
+const stream = {
+    write: (message: string) => logger.http(message.trim()),
+};
+app.use(morgan("dev", { stream }));
+
+//app.use("/api/user", authRoutes);
+
+app.use(ErrorHandlingMiddleware);
+
+export default app;
