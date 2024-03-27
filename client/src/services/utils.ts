a459e5429253810805b5e43087d08c4b92e9654d
Raazi Muhammed K <raazi6163@gmail.com>
2024-03-27 10:49:01 +0530

    chore: moved api urls into .env

--- client/src/services/utils.ts
+++ client/src/services/utils.ts
@@ -1,39 +1,70 @@
 import { AxiosError, AxiosResponse } from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const USER_SERVICE_URL = process.env.NEXT_PUBLIC_USER_SERVICE_URL;
+const PROJECT_SERVICE_URL = process.env.NEXT_PUBLIC_PROJECT_SERVICE_URL;
+const TEAM_SERVICE_URL = process.env.NEXT_PUBLIC_TEAM_SERVICE_URL;
+const API_DOC_SERVICE_URL = process.env.NEXT_PUBLIC_API_DOC_SERVICE_URL;
+const COMMUNICATION_SERVICE_URL =
+    process.env.NEXT_PUBLIC_COMMUNICATION_SERVICE_URL;
+const DB_DESIGN_SERVICE_URL = process.env.NEXT_PUBLIC_DB_DESIGN_SERVICE_URL;
+
 export class BuildUrl {
     private baseUrl: string;
 
     constructor() {
-        this.baseUrl = "http://";
+        if (!BASE_URL) throw new Error("Missing Base URL");
+        this.baseUrl = BASE_URL;
         return this;
     }
 
     user(endpoint: string) {
-        const url = "localhost:4000/api/user";
+        if (!USER_SERVICE_URL) {
+            throw new Error("Missing User Service URL");
+        }
+        const url = USER_SERVICE_URL;
         return this.baseUrl + url + endpoint;
     }
     project(endpoint: string) {
-        const url = "localhost:4005/api/project";
+        if (!PROJECT_SERVICE_URL) {
+            throw new Error("Missing Project Service URL");
+        }
+        const url = PROJECT_SERVICE_URL;
         return this.baseUrl + url + endpoint;
     }
     team(endpoint: string) {
-        const url = "localhost:4010/api/team";
+        if (!TEAM_SERVICE_URL) {
+            throw new Error("Missing Team Service URL");
+        }
+        const url = TEAM_SERVICE_URL;
         return this.baseUrl + url + endpoint;
     }
     apiDoc(endpoint: string) {
-        const url = "localhost:4015/api/api-doc";
+        if (!API_DOC_SERVICE_URL) {
+            throw new Error("Missing API Doc Service URL");
+        }
+        const url = API_DOC_SERVICE_URL;
         return this.baseUrl + url + endpoint;
     }
     module(endpoint: string) {
+        if (!PROJECT_SERVICE_URL) {
+            throw new Error("Missing Module Service URL");
+        }
         const url = "localhost:4020/api/module";
         return this.baseUrl + url + endpoint;
     }
     communication(endpoint: string) {
-        const url = "localhost:4025/api/communication";
+        if (!COMMUNICATION_SERVICE_URL) {
+            throw new Error("Missing Communication Service URL");
+        }
+        const url = COMMUNICATION_SERVICE_URL;
         return this.baseUrl + url + endpoint;
     }
     dbDesign(endpoint: string) {
-        const url = "localhost:4030/api/db-design";
+        if (!DB_DESIGN_SERVICE_URL) {
+            throw new Error("Missing DB Design Service URL");
+        }
+        const url = DB_DESIGN_SERVICE_URL;
         return this.baseUrl + url + endpoint;
     }
 }
