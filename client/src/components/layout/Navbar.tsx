e405eccc6261a2d62d9f7e5d0c0b35738c8a2e4b
Raazi Muhammed K <raazi6163@gmail.com>
2024-03-29 12:22:20 +0530

    style(navbar): change navbar design

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -5,14 +5,26 @@ import Container from "./Container";
 import { AppDispatch, useAppSelector } from "@/redux/store";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { logUser } from "@/redux/features/authSlice";
 import { useToast } from "../ui/use-toast";
-import { UserDropDownMenu } from "./UserDropDownMenu";
 import { UserService } from "@/services/api/user.service";
 import { makeApiCall } from "@/lib/apicaller";
 import { IResponse } from "@/services/api/utils";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { Label } from "../ui/label";
+import {
+    CircleUserRound as ProfileIcon,
+    WalletCards as ProjectIcon,
+    LogOut as LogOutIcon,
+} from "lucide-react";
 
 function Navbar() {
     const { toast } = useToast();
@@ -35,23 +47,74 @@ function Navbar() {
                 <nav className="my-auto ms-auto flex w-fit gap-8">
                     <div className="ms-auto flex gap-4">
                         {userData.userData ? (
-                            <>
-                                <div className="rounded-full border border-primary">
-                                    <Avatar
-                                        name={userData.userData.name}
-                                        src={userData.userData.avatar || ""}
-                                    />
-                                </div>
-                                <div>
-                                    <p>{userData.userData?.name}</p>
-                                    <small className="-mt-1 flex text-secondary">
-                                        {userData.userData?.email}
-                                    </small>
-                                </div>
-                                <UserDropDownMenu
-                                    username={userData.userData.username}
-                                />
-                            </>
+                            <NavigationMenu>
+                                <NavigationMenuList>
+                                    <NavigationMenuItem>
+                                        <NavigationMenuTrigger className="flex h-full gap-2">
+                                            <div className="flex gap-4 rounded-full border border-primary">
+                                                <Avatar
+                                                    name={
+                                                        userData.userData.name
+                                                    }
+                                                    src={
+                                                        userData.userData
+                                                            .avatar || ""
+                                                    }
+                                                />
+                                            </div>
+                                            <div>
+                                                <p>{userData.userData?.name}</p>
+                                                <small className="-mt-1 flex text-secondary">
+                                                    {userData.userData?.email}
+                                                </small>
+                                            </div>
+                                        </NavigationMenuTrigger>
+                                        <NavigationMenuContent className="w-full p-2">
+                                            <section className="w-full rounded-sm p-2 hover:bg-muted">
+                                                <div className="flex gap-2">
+                                                    <ProfileIcon
+                                                        size="1.2rem"
+                                                        className="my-auto"
+                                                    />
+                                                    <p className="my-auto">
+                                                        Profile
+                                                    </p>
+                                                </div>
+                                                <Label>
+                                                    Edit and view your profile
+                                                    details effortlessly
+                                                </Label>
+                                            </section>
+                                            <section className="w-44 rounded-sm p-2 hover:bg-muted">
+                                                <div className="flex gap-2">
+                                                    <ProjectIcon
+                                                        size="1.2rem"
+                                                        className="my-auto"
+                                                    />
+                                                    <p className="my-auto">
+                                                        Projects
+                                                    </p>
+                                                </div>
+                                                <Label>
+                                                    Access and review all your
+                                                    projects in one place
+                                                </Label>
+                                            </section>
+                                            <section className="w-44 rounded-sm border-destructive-border p-2 hover:border hover:bg-destructive-to">
+                                                <div className="flex gap-2">
+                                                    <LogOutIcon
+                                                        size="1.2rem"
+                                                        className="my-auto"
+                                                    />
+                                                    <p className="my-auto">
+                                                        Logout
+                                                    </p>
+                                                </div>
+                                            </section>
+                                        </NavigationMenuContent>
+                                    </NavigationMenuItem>
+                                </NavigationMenuList>
+                            </NavigationMenu>
                         ) : (
                             <Link href="/login">
                                 <Button>Login</Button>
