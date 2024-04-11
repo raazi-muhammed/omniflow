df9fa3d8c6e859ba987fdd711cebc3062967d585
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-11 12:55:57 +0530

    chore(db-design): setup jest

---
+++ db-design/.jest/setEnvVars.js
@@ -0,0 +1,8 @@
+process.env.PORT = 4030;
+process.env.SERVER_NAME = "db-design";
+process.env.CORS_ORIGINS = "https://example.com";
+process.env.ACCESS_TOKEN_SECRET = "your_access_token_secret";
+process.env.KAFKA_BROKER = "broker";
+process.env.KAFKA_CLIENT_ID = "client id";
+process.env.DB_URL = "db-url";
+process.env.NODE_ENV = "test";
---
+++ db-design/src/entities/__tests__/relation.entity.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "@jest/globals";
+
+describe("header entity", () => {
+    it("should return header data with only required data", () => {
+        expect(1).toEqual(1);
+    });
+});
