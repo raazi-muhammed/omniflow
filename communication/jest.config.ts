f14e5752dc9fafca7a5accb8b00df96285e2a307
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-11 13:18:50 +0530

    chore(communication): setup jest

---
+++ communication/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "jest";
+
+const config: Config = {
+    clearMocks: true,
+    verbose: true,
+    collectCoverage: true,
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
+    },
+    moduleNameMapper: {
+        "(.+)\\.js": "$1",
+    },
+    setupFiles: ["<rootDir>/.jest/setEnvVars.js"],
+    testPathIgnorePatterns: ["__tests__/index*"],
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+};
+
+export default config;
---
+++ communication/src/entities/__tests__/meeting.entity.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "@jest/globals";
+
+describe("meeting entity", () => {
+    it("meeting return module data with valid data", () => {
+        expect(1).toEqual(1);
+    });
+});
