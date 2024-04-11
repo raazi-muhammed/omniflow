9882b1e7094420ec1901c8fbed4e1c8865136c50
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-11 12:02:21 +0530

    test(api-doc): setup jest

---
+++ api-doc/.jest/setEnvVars.js
@@ -0,0 +1,8 @@
+process.env.PORT = 4015;
+process.env.SERVER_NAME = "api-doc";
+process.env.CORS_ORIGINS = "https://example.com";
+process.env.ACCESS_TOKEN_SECRET = "your_access_token_secret";
+process.env.KAFKA_BROKER = "broker";
+process.env.KAFKA_CLIENT_ID = "client id";
+process.env.DB_URL = "db-url";
+process.env.NODE_ENV = "test";
---
+++ api-doc/src/entities/__tests__/endpoint.entity.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "@jest/globals";
+
+describe("task entity", () => {
+    it("should return module data with valid data", () => {
+        expect(1).toBe(1);
+    });
+});
