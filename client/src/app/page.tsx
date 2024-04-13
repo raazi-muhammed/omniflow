430e08f3cb827e5a0abc122e033a692eeee38294
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-13 15:01:37 +0530

    chore: add metadata

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,3 @@
-import Heading from "@/components/custom/Heading";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { LandingPage } from "./_components/LandingPage";
 import Navbar from "@/components/layout/Navbar";
 
