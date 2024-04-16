566b0060acc3a6441763a2b0411b00f4f7c2299b
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-16 14:09:09 +0530

    refactor: add env check

--- team/src/index.ts
+++ team/src/index.ts
@@ -4,5 +4,7 @@ import "./config/index.js";
 
 const { PORT } = loadEnv(["PORT", "SERVER_NAME"]);
 app.listen(PORT, () => {
+    if (!PORT) console.log("No port");
+
     logger.info(`Server started\t: http://localhost:${PORT}`);
 });
