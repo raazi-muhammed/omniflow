f9f90a5f8e81964376e8ba4a08f5a60d5e65bd47
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-16 13:58:59 +0530

    refactor: remove unwanted env check

--- team/src/index.ts
+++ team/src/index.ts
@@ -2,11 +2,7 @@ import app from "./app.js";
 import { loadEnv, logger } from "@omniflow/common";
 import "./config/index.js";
 
-const { PORT, SERVER_NAME } = loadEnv(["PORT", "SERVER_NAME"]);
+const { PORT } = loadEnv(["PORT", "SERVER_NAME"]);
 app.listen(PORT, () => {
-    if (!PORT) {
-        logger.error(`PORT NOT FOUND ON ${SERVER_NAME.toUpperCase()}`);
-        return;
-    }
     logger.info(`Server started\t: http://localhost:${PORT}`);
 });
