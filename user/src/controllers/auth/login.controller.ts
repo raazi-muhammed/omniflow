76e3b769bd2fa4d8cacf4ac2c7fedd1f5389f733
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-18 14:37:32 +0530

    fix: cookie not setting properly

--- user/src/controllers/auth/login.controller.ts
+++ user/src/controllers/auth/login.controller.ts
@@ -22,11 +22,17 @@ export default function buildLoginController({
             password: inputData.password,
         });
 
+        const expirationDate = new Date();
+        expirationDate.setMonth(expirationDate.getMonth() + 1);
+
+        // Convert the expiration date to UTC string format
+        const expires = expirationDate.toUTCString();
+
         const response = new ResponseCreator();
         return response
             .setData(user)
             .setHeaders({
-                "Set-Cookie": `${TOKEN_COOKIE_NAME}=${token}; Path=/`,
+                "Set-Cookie": `${TOKEN_COOKIE_NAME}=${token}; Path=/; Expires=${expires}; HttpOnly`,
             })
             .setStatusCode(200)
             .setMessage("Login successful");
