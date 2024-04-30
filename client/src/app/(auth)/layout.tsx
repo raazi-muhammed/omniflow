a8bdebf847dde2fff22914f96fd639961b254de7
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-30 18:18:37 +0530

    chore: add private routes

--- client/src/app/(auth)/layout.tsx
+++ client/src/app/(auth)/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Navbar from "@/components/layout/Navbar";
 import { toast } from "@/components/ui/use-toast";
 import { useAppSelector } from "@/redux/store";
 import { useRouter } from "next/navigation";
@@ -14,14 +13,9 @@ export default function layout({ children }: Props) {
     const router = useRouter();
     const { isLoading, isAuth } = useAppSelector((state) => state.authReducer);
 
-    if (!isLoading && !isAuth) {
-        router.push("/login");
-        toast({ variant: "destructive", description: "You are not logged in" });
+    if (!isLoading && isAuth) {
+        router.push("/projects");
+        toast({ description: "You are already logged in" });
     }
-    return (
-        <main className="min-h-screen bg-gradient-to-tr from-[#100730] from-0% via-black via-30% to-[#100730] to-100%">
-            <Navbar />
-            {children}
-        </main>
-    );
+    return children;
 }
